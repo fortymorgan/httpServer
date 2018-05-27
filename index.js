b586489f841bf6c5ad25e06deaf5cfdb8fa05525
Forty Morgan <dimazhmudikov@gmail.com>
2018-05-27 20:29:09 +0300

    Generating response on request data

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 #!/usr/bin/env node
 const net = require('net');
 
-const response = `HTTP/1.1 200 OK
-Content-type: text/plain
-Content-length: 13
-
-Hello, World!`
-
 const server = new net.Server;
 server.on('connection', socket => {
   socket.setEncoding('utf8');
   socket.on('data', data => {
+    const [query, ...headers] = data.split('\r\n');
+    // console.log(query);
+    const [verb, path, version] = query.split(' ');
+    const headersArray = headers.filter(el => !!el).map(header => header.split(': '));
+    // console.log(headersArray);
+    const headersHtmlTableArray = headersArray.map(item => {
+      const [header, value] = item;
+      return `<tr><td>${header}</td><td>${value}</td></tr>`
+    });
+    const htmlTableHeaders = '<tr><th>Header</th><th>Value</th></tr>';
+    const content = `<p>Verb: ${verb}</p>
+    <p>Path: ${path}</p>
+    <p>Version: ${version.split('/').join(' ')}
+    <table border="1">
+    ${[htmlTableHeaders, ...headersHtmlTableArray].join('\n')}
+    </table>`
+    const response = `${version} 200 OK
+Content-type: text/html
+Content-length: ${Buffer.byteLength(content)}
+
+${content}`
     socket.write(response);
   });
 });
