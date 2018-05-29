e72959591ad25ae5b3fa9f5f584a8e1dfbc308e3
Forty Morgan <dimazhmudikov@gmail.com>
2018-05-29 15:50:47 +0300

    Add request validation and response generation functions

--- index.js
+++ index.js
@@ -1,26 +1,99 @@
 #!/usr/bin/env node
 const net = require('net');
-let dataContainer = '';
 
-const server = new net.Server;
-server.on('connection', socket => {
+const isRequestOver = data => data.includes('\r\n\r\n')
+
+const isRequestValid = data => {
+  const query = getQueryLine(data);
+  const verbs = ['GET', 'HEAD'];
+  const [verb, path, version] = query.split(' ');
+
+  const valid = verbs.includes(verb.toUpperCase())
+    && path[0] === '/'
+    && version.toUpperCase().includes('HTTP/');
+  
+  return valid;
+}
+
+const getQueryLine = data => {
+  const [query] = data.split('\r\n');
+  return query;
+}
+
+const getVersion = data => {
+  const query = getQueryLine(data);
+  const [_, __, version] = query.split(' ');
+  return version.toUpperCase();
+}
+
+const generateResponseHeaders = (headers, body) => {
+  const headerTypes = {
+    'Content-type': () => 'text/html',
+    'Content-length': () => Buffer.byteLength(body),
+  }
+  return headers.map(header => `${header}: ${headerTypes[header]()}`).join('\n')
+}
+
+const generateResponse = (code, data, version = 'HTTP/1.0') => {
+  const responses = {
+    '200': {
+      message: 'OK',
+      headers: ['Content-type', 'Content-length'],
+      body: data => generateOkBody(data),
+    },
+    '400': {
+      message: 'Bad Request',
+      headers: ['Content-type', 'Content-length'],
+      body: function() { return `<h1>${this.code} ${this.message}</h1>` },
+    }
+  }
+
+  const response = responses[code];
+
+  const responseHead = `${version} ${code} ${response.message}`;
+  const responseBody = response.body(data);
+  const responseHeaders = generateResponseHeaders(response.headers, responseBody);
+  return `${responseHead}\n${responseHeaders}\n\n${responseBody}`;
+}
+
+const generateHeadersHtmlTable = headers => {
+  const headersArray = headers.filter(el => !!el).map(header => header.split(': '));
+  const headersHtmlTableArray = headersArray.map(item => {
+    const [header, value] = item;
+    return `<tr><td>${header}</td><td>${value}</td></tr>`
+  });
+  const htmlTableHeaders = '<tr><th>Header</th><th>Value</th></tr>';
+  return `<table border>\n${[htmlTableHeaders, ...headersHtmlTableArray].join('\n')}\n</table>`;
+}
+
+const generateQueryHtmlData = queryLine => {
+  const [verb, path, version] = queryLine.split(' ');
+  const verbHtmlString = `<p>Verb: ${verb.toUpperCase()}</p>`;
+  const pathHtmlString = `<p>Path: ${path}</p>`;
+  const versionHtmlString = `<p>Version: ${version.toUpperCase().split('/').join(' ')}</p>`;
+  return `${verbHtmlString}${pathHtmlString}${versionHtmlString}`;
+}
+
+const generateOkBody = data => {
+  const [query, ...headers] = data.split('\r\n');
+  const queryHtmlData = generateQueryHtmlData(query);
+  const headersHtmlTable = generateHeadersHtmlTable(headers);
+  return `${queryHtmlData}\n${headersHtmlTable}`;
+}
+
+net.createServer(socket => {
+  let dataContainer = '';
   socket.setEncoding('utf8');
   socket.on('data', data => {
     dataContainer += data;
-    if (dataContainer.includes('\r\n\r\n')) {
-      const [query, ...headers] = dataContainer.split('\r\n');
-      const [verb, path, version] = query.split(' ');
-      const headersArray = headers.filter(el => !!el).map(header => header.split(': '));
-      const headersHtmlTableArray = headersArray.map(item => {
-        const [header, value] = item;
-        return `<tr><td>${header}</td><td>${value}</td></tr>`
-      });
-      const htmlTableHeaders = '<tr><th>Header</th><th>Value</th></tr>';
-      const content = `<p>Verb: ${verb}</p><p>Path: ${path}</p><p>Version: ${version.split('/').join(' ')}</p><table border="1">${[htmlTableHeaders, ...headersHtmlTableArray].join('')}</table>`;
-      const response = `${version} 200 OK\nContent-type: text/html\nContent-length: ${Buffer.byteLength(content)}\n\n${content}`;
-      socket.write(response, () => dataContainer = '');
+    if (!isRequestValid(dataContainer)) {
+      const response400 = generateResponse(400);
+      socket.write(response400, () => socket.end());
+    }
+    if (isRequestOver(dataContainer)) {
+      const version = getVersion(dataContainer);
+      const response200 = generateResponse(200, dataContainer, version);
+      socket.write(response200, () => dataContainer = '');
     }
   });
-});
-
-server.listen(3000);
+}).listen(3000);
